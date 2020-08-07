eca4b6e5f260152f4929754c0e12a34b1ea22478
Namratha-22 <54387027+Namratha-22@users.noreply.github.com>
2020-08-07 12:22:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import  Keys
-from time import sleep, strftime
+
 
 
 def login():
