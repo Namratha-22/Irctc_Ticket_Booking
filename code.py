7134d43105e8550a137f960c40ea7331faf593af
Namratha-22 <54387027+Namratha-22@users.noreply.github.com>
2020-08-07 12:20:09 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import  Keys
 from time import sleep, strftime
 
 def waituntil(s):
@@ -12,45 +13,37 @@ def waituntil(s):
 
 def login():
     driver.get('https://www.irctc.co.in/eticketing/loginHome.jsf')
-    WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.NAME, 'j_username'))
-        ).send_keys(IRCTC_USERNAME)
+    wait=WebDriverWait(driver, 60)
+    wait.until(EC.presence_of_element_located((By.NAME, 'j_username'))).send_keys(IRCTC_USERNAMEW)
     driver.find_element_by_name('j_password').send_keys(IRCTC_PASSWORD)
     driver.find_element_by_name('j_captcha').send_keys('')
 
 def planjourney():
     WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.ID, 'jpform:fromStation'))
-        ).send_keys(FROM_STATION)
+        EC.presence_of_element_located((By.ID, 'jpform:fromStation'))).send_keys(FROM_STATION)
     driver.find_element_by_id('jpform:toStation').send_keys(TO_STATION)
     driver.find_element_by_id('jpform:journeyDateInputDate').send_keys(DATE)
-    #waituntil('10:00:05')
-    driver.find_element_by_id('jpform:jpsubmit').click()
-    WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.NAME, 'quota'))
-        )[0].click()
-    driver.find_element_by_id('cllink-%s-%s-%s' % (TRAIN_NO, CLASS, CLASS_INDEX)).click()
-    # WebDriverWait(driver, 60).until(
-    #     EC.presence_of_all_elements_located((By.ID, '%s-%s-CK-0' % (TRAIN_NO, CLASS)))
-    #     )[-1].click()
+    train_nums=driver.find_elements_by_xpath("//div/h3[@class='ico22 black fontQuick']")
+    for i in train_nums:
+        if i == TRAIN_NO :
+            driver.find_element_by_id('jpform:jpsubmit').click() 
+    
 
 def filldetails():
     WebDriverWait(driver, 60).until(EC.title_contains('Book Ticket'))
-    for name, el in zip(NAMES, driver.find_elements_by_class_name('psgn-name')):
+    for name, el in zip(NAME, driver.find_elements_by_class_name('psgn-name')):
         el.send_keys(name)
     for age, el in zip(AGES, driver.find_elements_by_class_name('psgn-age')):
         el.send_keys(age)
     for gender, el in zip(GENDERS, driver.find_elements_by_class_name('psgn-gender')):
         Select(el).select_by_value(gender)
-    for berth, el in zip(BERTHS, driver.find_elements_by_class_name('psgn-berth-choice')):
+    for berth, el in zip(BERTHS, driver.find_elements_by_clas_name('psgn-berth-choice')):
         Select(el).select_by_value(berth)
+    driver.find_element_by_xpath("//div/button[text()='SAVE']").click()     
     
 
 def sbi():
-    WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.ID, 'PREFERRED'))
-        )[-1].click()
-    driver.find_element_by_id('validate').click()
+    driver.find_element_by_id('//div/button[text()="Pay ₹ 200"]').click()
     WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.ID, 'debitCardNumber'))
         ).send_keys(SBI_CARD_NUMBER)
@@ -58,8 +51,7 @@ def sbi():
     driver.find_element_by_id('debiMonth').send_keys(SBI_CARD_EXPIRY_MM)
     driver.find_element_by_id('debiYear').send_keys(SBI_CARD_EXPIRY_YYYY)
     driver.find_element_by_id('cardPin').send_keys(SBI_CARD_PIN)
-    #for MONTH, el in zip(SBI_CARD_EXPIRY_MM, driver.find_elements_by_class_name('tbox')):
-        #Select(el).select_by_value(MONTH)
+   
     
 
 
@@ -77,7 +69,7 @@ NAMES = ['NAME1'] #Passenger name list
 AGES = ['20']            #Passenger age list
 GENDERS = ['M']          #Passenger sex list
 BERTHS = ["  "]          #Passenger preference list. Double space represent no preference.    
-''
+
 SBI_CARD_NUMBER = ''
 SBI_CARD_EXPIRY_MM=''
 SBI_CARD_EXPIRY_YYYY=''
@@ -87,7 +79,7 @@ SBI_CARD_NAME=''
 if __name__ == '__main__':
     profile =webdriver.Chrome(executable_path="C:\python practice\py_driver\chromedriver_win32\chromedriver.exe")
     profile.set_preference('webdriver.load.strategy', 'unstable')
-    #waituntil('10:00:05')
+ 
     driver = webdriver.Firefox(profile)
     login()
     planjourney()
